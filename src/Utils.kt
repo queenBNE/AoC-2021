130acdcfb6db8a50ae192f9349dbbf359a88263b
jacobien <jacobienc@gmail.com>
2021-11-30 21:20:14 +0100

    Move data to data folder

--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,12 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Reads lines from input file and returns list of strings.
+ */
+fun readLinesFromFile(fileName: String): List<String>
+        = File(fileName).useLines { it.toList() }
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
